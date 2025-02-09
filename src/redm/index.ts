4bf6e80cad9d1396d4cdc3ea16cf4f39993ed50e
AvarianKnight <dillon@dillonskaggs.dev>
2025-02-09 12:15:44 -0600

    fix(redm): fix raw controls not being added

--- src/redm/index.ts
+++ src/redm/index.ts
@@ -2,6 +2,8 @@ export * from "./entities/index";
 export * from "./enums/index";
 export * from "./Volume";
 export * from "./Controls";
+export * from "./RawControls";
+
 export * from "./Game";
 
 export * from "./world";
