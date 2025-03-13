cf0802c08361c9e70d3bfa9754f3850a1dd08cc1
AvarianKnight <dillon@dillonskaggs.dev>
2025-03-12 19:20:39 -0500

    tweak(volume): add multiple volume types

--- src/redm/Volume.ts
+++ src/redm/Volume.ts
@@ -1,25 +1,36 @@
 import type { Vector3 } from "@common/utils/Vector";
 
-export class Volume {
-  private handle: number;
-  constructor(coord: Vector3, rot: Vector3, scale: Vector3, customName?: string) {
-    if (customName) {
-      this.handle = CreateVolumeCylinderWithCustomName(
-        coord.x,
-        coord.y,
-        coord.z,
-        rot.x,
-        rot.y,
-        rot.z,
-        scale.x,
-        scale.y,
-        scale.z,
-        customName as unknown as number,
-      );
-      return;
+export enum VolumeType {
+  Cylinder,
+  Box
+}
+
+
+function getVolumeFunctionFromType(volumeType: VolumeType, hasCustomName: boolean) {
+  if (hasCustomName) {
+    switch (volumeType) {
+      case VolumeType.Cylinder: return CreateVolumeCylinderWithCustomName
+      case VolumeType.Box: return CreateVolumeBoxWithCustomName;
     }
 
-    this.handle = CreateVolumeCylinder(coord.x, coord.y, coord.z, rot.x, rot.y, rot.z, scale.x, scale.y, scale.z);
+  } else {
+
+    switch (volumeType) {
+      case VolumeType.Cylinder: return CreateVolumeCylinder;
+
+      case VolumeType.Box: return CreateVolumeBox;
+    }
+
+  }
+
+}
+
+
+export class Volume {
+  private handle: number;
+  constructor(volumeType: VolumeType, coord: Vector3, rot: Vector3, scale: Vector3, customName?: string) {
+    const fn = getVolumeFunctionFromType(volumeType, customName !== undefined);
+    this.handle = fn(coord.x, coord.y, coord.z, rot.x, rot.y, rot.z, scale.x, scale.y, scale.z, customName as unknown as any);
   }
 
   get Handle(): number {
