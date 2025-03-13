23b80d6bf2b6653006387ee1160ea721947d37ea
AvarianKnight <dillon@dillonskaggs.dev>
2025-03-12 19:22:15 -0500

    fix ci
    it didn't work i need to fix

--- src/redm/Volume.ts
+++ src/redm/Volume.ts
@@ -2,35 +2,44 @@ import type { Vector3 } from "@common/utils/Vector";
 
 export enum VolumeType {
   Cylinder,
-  Box
+  Box,
 }
-
 
 function getVolumeFunctionFromType(volumeType: VolumeType, hasCustomName: boolean) {
   if (hasCustomName) {
     switch (volumeType) {
-      case VolumeType.Cylinder: return CreateVolumeCylinderWithCustomName
-      case VolumeType.Box: return CreateVolumeBoxWithCustomName;
+      case VolumeType.Cylinder:
+        return CreateVolumeCylinderWithCustomName;
+      case VolumeType.Box:
+        return CreateVolumeBoxWithCustomName;
     }
-
   } else {
-
     switch (volumeType) {
-      case VolumeType.Cylinder: return CreateVolumeCylinder;
+      case VolumeType.Cylinder:
+        return CreateVolumeCylinder;
 
-      case VolumeType.Box: return CreateVolumeBox;
+      case VolumeType.Box:
+        return CreateVolumeBox;
     }
-
   }
-
 }
-
 
 export class Volume {
   private handle: number;
   constructor(volumeType: VolumeType, coord: Vector3, rot: Vector3, scale: Vector3, customName?: string) {
     const fn = getVolumeFunctionFromType(volumeType, customName !== undefined);
-    this.handle = fn(coord.x, coord.y, coord.z, rot.x, rot.y, rot.z, scale.x, scale.y, scale.z, customName as unknown as any);
+    this.handle = fn(
+      coord.x,
+      coord.y,
+      coord.z,
+      rot.x,
+      rot.y,
+      rot.z,
+      scale.x,
+      scale.y,
+      scale.z,
+      customName as unknown as any,
+    );
   }
 
   get Handle(): number {
