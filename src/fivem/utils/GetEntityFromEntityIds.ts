7bf8b865932f01ece3605354dbfc9d1e10e475cd
AvarianKnight <dillon@dillonskaggs.dev>
2025-03-17 15:30:35 -0500

    tweak(fivem): remove un-needed base entity ref

--- src/fivem/utils/GetEntityFromEntityIds.ts
+++ src/fivem/utils/GetEntityFromEntityIds.ts
@@ -1,11 +1,9 @@
-import { BaseEntity } from "fivem/models";
 import type { Entity } from "fivem/models/Entity";
 import { Ped } from "fivem/models/Ped";
 import { Prop } from "fivem/models/Prop";
 import { Vehicle } from "fivem/models/Vehicle";
 
 export function GetEntityClassFromId(entityId: number): Entity {
-  BaseEntity;
   switch (GetEntityType(entityId)) {
     case 1: {
       return Ped.fromHandle(entityId);
