daad98c90a6d7eef1002e07ada3fdfcca44b3b28
Dillon Skaggs <dillon@dillonskaggs.dev>
2026-02-10 23:21:00 -0600

    fix(common): randomInt should return an int

--- src/common/utils/randomInt.ts
+++ src/common/utils/randomInt.ts
@@ -1,3 +1,7 @@
-export function randomInt(min: number, max: number) {
+export function randomFloat(min: number, max: number) {
   return Math.random() * (max - min) + min;
+}
+
+export function randomInt(min: number, max: number) {
+  return Math.floor(randomFloat(min, max));
 }
